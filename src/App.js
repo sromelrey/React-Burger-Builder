d89bb14e3a23dbc0d474fabf5b827cbf088c36c4
unknown <sromelrey>
2020-09-20 18:43:29 +0800

    Burger builder Initial Commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import Layout from './Layout/Layout';
+import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
+import './App.css';
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+     <Layout>
+      <BurgerBuilder/>
+     </Layout>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -0,0 +1,35 @@
+import React,{Fragment} from "react";
+import Logo from "../../Logo/Logo";
+import NavigationsItems from "../NavigationItems/NavigationItems";
+import Backdrop from "../../UI/Backdrop/Backdrop";
+
+
+import classes from "./SideDrawer.css";
+
+const sideDrawer = (props) => {
+  let attachedClasses = [classes.SideDrawer,classes.Close];
+  
+  if(props.open){
+    attachedClasses = [classes.SideDrawer,classes.Open];
+  }
+
+  return (
+    <Fragment>
+      <Backdrop 
+      show={props.open}
+      clicked={props.closed}/>
+
+      <div className={attachedClasses.join(' ')}>
+        <div className={classes.Logo}>
+          <Logo />
+        </div>
+
+        <nav>
+          <NavigationsItems />
+        </nav>
+      </div>
+    </Fragment>
+  );
+};
+
+export default sideDrawer;
